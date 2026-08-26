c9b41bc4e58cab44bb5abe805e6d613f46ce449e
Babli <bablirawat172005@gmail.com>
2026-08-26 09:55:52 +0530

    Time: 135 ms (13.24%), Space: 20.2 MB (99.43%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def findDuplicate(self, nums):
-        See = set()
-
-        for i in range(len(nums)):
-            if nums[i] in See:
-                return nums[i]
-            See.add(nums[i])
+     nums.sort()
+     for i in range (len(nums)):
+         if nums[i]==nums[i+1]:
+            return nums[i]
+     return None
